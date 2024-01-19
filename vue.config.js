358cd056a58d41be36b7bb6fe0f6b3eea53bce9c
Henry Popp <henry@codedge.io>
2024-01-19 14:30:25 -0600

    fix: cname in actions input

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  base: "/tracklist-formatter/",
-  transpileDependencies: true,
-});
+
+module.exports = {
+  publicPath: "/tracklist-formatter/",
+};
