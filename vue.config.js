5bf227c1613e825063b5d5b7075afd6eaeb0c98a
Henry Popp <henry@codedge.io>
2024-01-19 14:38:55 -0600

    fix: revert to old vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 
-module.exports = {
-  publicPath: "/tracklist-formatter/",
+module.exports = defineConfig({
   transpileDependencies: true,
-};
+});
